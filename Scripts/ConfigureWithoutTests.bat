892917a9114188f99a7339c27bf4c020b85558c4
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2026-08-22 17:59:37 +0900

    remove scripts

--- Scripts/ConfigureWithoutTests.bat
+++
@@ -1,9 +0,0 @@
-cd ..
-if not exist "Build" mkdir Build
-
-set miniconda_activate=C:\Users\%USERNAME%\miniconda3\Scripts\activate.bat
-call %miniconda_activate% base
-
-cmake --preset "Windows Visual Studio" -DBUILD_TESTS=OFF
-
-pause> nul
